518ea6415933dec7d7324aae56dd40c305a0f9d9
Cho Xuan Xian <1201302646@student.mmu.edu.my>
2023-08-28 11:40:22 +0800

    firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { collection, getDocs, getFirestore, query, where } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAJEBS_KSaMtfvE80t-4D-dvBA909LfHVA",
@@ -18,7 +24,9 @@ export const db = getFirestore(app);
 
 export const getDocuments = async (collectionName, userId) => {
   console.log("asdfs");
-  const docs = await getDocs(query(collection(db, collectionName), where("userId", "==", userId)));
+  const docs = await getDocs(
+    query(collection(db, collectionName), where("userId", "==", userId))
+  );
   console.log("asdfs1");
   const docslists = docs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
   return docslists;
